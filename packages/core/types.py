e34632f90f13baa9122ea869311202a34bc81b23
Sanjay G <gokulesh2615@gmail.com>
2026-05-24 16:37:36 +0530

    refactor(core): handle missing agent in task scheduling and add type annotations to Agent class

--- packages/core/types.py
+++ packages/core/types.py
@@ -14,3 +14,26 @@ class PoolOrchestratorConfig:
 
     def __str__(self) -> str:
         return f'PoolOrchestratorConfig(monitor_interval={self.monitor_interval})'
+
+class Agent:
+    def __init__(self, id: str, name: str, capabilities: List[str]):
+        self.id = id
+        self.name = name
+        self.capabilities = capabilities
+
+    def can_handle(self, task: 'Task') -> bool:
+        # This is a very simplified example of the can_handle function
+        # In a real world scenario, this function would be more complex
+        return task.requirements[0] in self.capabilities
+
+class Task:
+    def __init__(self, id: str, name: str, requirements: List[str], dependencies: List[str]):
+        self.id = id
+        self.name = name
+        self.requirements = requirements
+        self.dependencies = dependencies
+
+class TaskDependency:
+    def __init__(self, task_id: str, dependencies: List[str]):
+        self.task_id = task_id
+        self.dependencies = dependencies
